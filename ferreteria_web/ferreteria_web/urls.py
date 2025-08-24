af4e117bf00d2e7008a5b000cd76dee65dd8dd43
ferreteria-center <ferreteria@gmail.com>
2025-08-24 18:03:13 -0500

    Actualizar proyecto con nueva estructura, eliminando archivos antiguos

--- ferreteria_web/ferreteria_web/urls.py
+++
@@ -1,8 +0,0 @@
-# ferreteria_web/urls.py (archivo del proyecto)
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('inventario.urls')),
-]
--- ferreteria_web/run.py
+++
@@ -1,6 +0,0 @@
-from waitress import serve
-from ferreteria_web.wsgi import application
-
-if __name__ == "__main__":
-    print("🚀 Servidor corriendo en http://0.0.0.0:8000")
-    serve(application, host="0.0.0.0", port=8000)
